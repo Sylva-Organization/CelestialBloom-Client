a124e7d7d101ef72efd8628ab33cb25c129078fa
guiss26 <guissella584@gmail.com>
2025-10-01 11:01:43 -0700

    feat(ui/home): implement homepage section with parallax and front cards (#26)

--- src/components/ArticlesBlog.jsx
+++ src/components/ArticlesBlog.jsx
@@ -1,10 +1,26 @@
 import './ArticlesBlog.css'
+import ArticlesFront from './ArticlesFront'
 
 const ArticlesBlog = () => {
     return (
         <>
-            
-
+            <section className="articles-section">
+                <article className="articles-container">
+                    <div className='box-article astronomy-c'>
+                        <h2>Astronomía</h2>
+                        <ArticlesFront></ArticlesFront>
+                        <ArticlesFront></ArticlesFront>
+                        <ArticlesFront></ArticlesFront>
+                    </div>
+                    <div className='box-article botany-c'>
+                        <h2>Botánica</h2>
+                        <ArticlesFront></ArticlesFront>
+                        <ArticlesFront></ArticlesFront>
+                        <ArticlesFront></ArticlesFront>
+                    </div>
+                </article>
+                {/* <ArticlesFront></ArticlesFront> */}
+            </section>
         </>
     )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
+import ArticlesBlog from '../components/ArticlesBlog'
+import Parallax from '../components/Parallax'
 import './Home.css'
 
 const Home = () => {
     return (
         <>
-            
-
+            <Parallax></Parallax>
+            <ArticlesBlog></ArticlesBlog>
         </>
     )
 }
