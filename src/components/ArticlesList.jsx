6e17912cb38d900194f204503630a5d5266e31eb
guiss26 <guissella584@gmail.com>
2025-10-07 09:25:58 -0700

    refactor: rename ArticleBlog to ArticlesList

---
+++ src/components/ArticlesList.jsx
@@ -0,0 +1,43 @@
+import './ArticlesList.css'
+import ArticlesFront from './ArticlesFront'
+import { Link } from 'react-router-dom';
+
+const ArticlesList = ({ users }) => {
+    //AQUÍ NO HAY QUE PASAR NINGÚN MÉTODO
+
+
+    // post botánica
+    // const botanyPosts = users.map(user => 
+    //     user.posts.filter(post => post.categories.name.toLowerCase() === "botánica")
+    // )
+
+    // posts astronomía
+    // const astronomyPosts = users.map(user => 
+    //     user.posts.filter(post => post.categories.name.toLowerCase() === "astronomía")
+    // )
+
+    return (
+        <>
+            <section className="filter-tabs">
+                <div className="container-tab">
+                    <div className="tab-buttons">
+                        <Link to="/" className='tab-btn active' data-filter="todos">Todos los Posts</Link>
+                        <Link to="/botanica" className='tab-btn' data-filter="botanica">🌿 Botánica</Link>
+                        <Link to="/astronomia" className='tab-btn' data-filter="astronomia">⭐ Astronomía</Link>
+                    </div>
+                </div>
+            </section>
+
+            <section className="articles-section">
+                <article className="articles-container">
+                    <div className='box-article'>
+                        <h2>Artículos</h2>
+                            <ArticlesFront></ArticlesFront>
+                    </div>
+                </article>
+            </section>
+        </>
+    )
+}
+
+export default ArticlesList
