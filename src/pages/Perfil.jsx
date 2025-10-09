10b8f02c12732b94a338396b4f635f062f54fa33
guiss26 <guissella584@gmail.com>
2025-10-09 14:34:57 -0700

    feat(components): add Perfil component with user info and posts section (#39)

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -3,16 +3,34 @@ import './Perfil.css'
 const Perfil = () => {
     return (
         <>
-            <section className="section-perfil">
-                <h1 className='title-perfil'>Perfil</h1>
-                <div className="perfil-container">
-                    <h3 className='name-perfil'>Yuleisy Pacheco</h3>
-                    <span className='role-perfil'>usuario</span>
-                    <p className="nickname-perfil">@layulei</p>
-                    <p className='email-perfil'>yuleisypa@yulei.com</p>
+            <section className="section-perfil-user">
+                <div className='perfil-container'>
+                    <div className="perfil-role">
+                        <h2 className='title-perfil'>Mi Perfil</h2>
+                        <span className='role-perfil'>usuario</span>
+                    </div>
+                    <hr />
+                    <div className="campos-user">
+                        <div className="campos-item">
+                            <span className="user-label">NOMBRE</span>
+                            <p className="user-name">Yuleisy</p>
+                        </div>
+                        <div className="campos-item">
+                            <span className="user-label">APELLIDOS</span>
+                            <p className="user-lastname">Pacheco Pérez</p>
+                        </div>
+                        <div className="campos-item">
+                            <span className="user-label">CORREO ELECTRÓNICO</span>
+                            <p className="user-email">yuleisypach@yuleisy.com</p>
+                        </div>
+                        <div className="campos-item">
+                            <span className="user-label">NICKNAME</span>
+                            <p className="user-nickname">@layulei</p>
+                        </div>
+                    </div>
                 </div>
-                <div className="posts-container">
-                    <h2 className='title-posts-client'>Mis posts</h2>
+                <div className='mis-posts-container'>
+                    <h2 className="title-mis-post">Mis posts</h2>
                     <hr />
                     <div className="posts-box">
                         <div className="posts-client">
