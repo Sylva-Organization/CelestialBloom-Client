1049b9f28539ef059bfb419ac23fc27d46018a18
gabriela-her <ari.ghb@gmail.com>
2025-10-16 18:25:41 +0200

    refactor(botany,astronomy):change clases lable to add style

--- src/pages/Astronomy.jsx
+++ src/pages/Astronomy.jsx
@@ -5,8 +5,8 @@ const Astronomy = () => {
     return (
         <>
             <section className="astronomy-section">
-                <h1 className="title-astronomy">Explora el universo que nos rodea</h1>
-                <h2 className="subtitle-astronomy">Aquí encontrarás todos nuestros artículos sobre astronomía, donde desentrañamos los misterios del espacio: planetas, estrellas, galaxias y mucho más.
+                <h1 className="page-title-astronomy">Explora el universo que nos rodea</h1>
+                <h2 className="page-subtitle-astronomy">Aquí encontrarás todos nuestros artículos sobre astronomía, donde desentrañamos los misterios del espacio: planetas, estrellas, galaxias y mucho más.
                     Aprende, explora y deja que la curiosidad te lleve más allá de la atmósfera.</h2>
                 <ArticlesFront categoryFilter="astronomia"></ArticlesFront>
             </section>
--- src/pages/Botany.jsx
+++ src/pages/Botany.jsx
@@ -5,8 +5,8 @@ const Botany = () => {
     return (
         <>
             <section className="botany-section">
-                <h1 className="title-botany">Descubre la vida que florece en la Tierra</h1>
-                <h2 >encontrarás todos los artículos dedicados a la botánica — el estudio de las plantas, su estructura, crecimiento y belleza natural.
+                <h1 className="page-title-botany">Descubre la vida que florece en la Tierra</h1>
+                <h2 className="page-subtitle-botany">Aquí encontrarás todos los artículos dedicados a la botánica — el estudio de las plantas, su estructura, crecimiento y belleza natural.
                     Desde las raíces hasta las copas de los árboles, explora cómo la vida verde sostiene nuestro planeta.</h2>
                 <ArticlesFront categoryFilter="botanica"></ArticlesFront>
             </section>
